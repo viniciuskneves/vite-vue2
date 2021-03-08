a04b8601aea28eb2792477ea85f3c62cb752fb97
Vinicius Kiatkoski Neves <viniciuskneves@icloud.com>
2021-03-08 15:42:45 +0100

    :tada: Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <hello-world msg="Vite + Vue 2" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: { HelloWorld },
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+
+    <p>
+      <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
+      <a href="https://vuejs.org/" target="_blank">Vue 2 Documentation</a>
+    </p>
+
+    <button @click="state.count++">count is: {{ state.count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test hot module replacement.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      state: {
+        count: 0,
+      },
+    };
+  },
+}
+</script>
